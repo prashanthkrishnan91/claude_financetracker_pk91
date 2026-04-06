017a7bbc0542a7d18727fe36102b2ef0cdae777e
prashanthkrishnan91 <prashanth.krishnan91@gmail.com>
2026-04-05 21:20:05 -0700

    Create ui_service.py

---
+++ services/ui_service.py
@@ -0,0 +1,9 @@
+# services/ui_service.py
+
+from uiux import render_dashboard as ui_render_dashboard
+
+def render_dashboard(data):
+    """
+    Wrapper for UI dashboard rendering
+    """
+    return ui_render_dashboard(data)

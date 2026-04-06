eebe761a31227f10ab975fd42482dfa4ea6c2583
prashanthkrishnan91 <prashanth.krishnan91@gmail.com>
2026-04-05 21:39:37 -0700

    Delete services directory

--- services/pattern_service.py
+++
@@ -1,6 +0,0 @@
-# services/pattern_service.py
-
-from patterns import apply_clean_code
-
-def clean_code(code):
-    return apply_clean_code(code)
--- services/ui_service.py
+++
@@ -1,9 +0,0 @@
-# services/ui_service.py
-
-from uiux import render_dashboard as ui_render_dashboard
-
-def render_dashboard(data):
-    """
-    Wrapper for UI dashboard rendering
-    """
-    return ui_render_dashboard(data)
